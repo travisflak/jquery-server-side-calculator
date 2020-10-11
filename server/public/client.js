a6f74667cd1e1172d069edecfa7372066bef5caa
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-10 22:16:13 -0500

    all click listeners for each button is properly listening

--- server/public/client.js
+++ server/public/client.js
@@ -4,17 +4,40 @@ function onReady() {
 
 
     $('#plus').on('click', plusBtnListener);
-    // $('#minus').on('click', postPlayerData);
-    // $('#times').on('click', postPlayerData);
-    // $('#divide').on('click', postPlayerData);
-    // $('#equals').on('click', postPlayerData);
-    // $('#C').on('click', postPlayerData);
+    $('#minus').on('click', minusBtnListener);
+    $('#times').on('click', timesBtnListener);
+    $('#divide').on('click', divideBtnListener);
+    $('#equals').on('click', equalsBtnListener);
+    $('#C').on('click', cBtnListener);
 }
 
 function plusBtnListener(){
  console.log('hello from plus button:', plusBtnListener);
- 
 
 }
 
+function minusBtnListener(){
+    console.log('hello from minus button:', minusBtnListener);
+   
+}
+
+function timesBtnListener(){
+    console.log('hello from times button:', timesBtnListener);
+   
+}
+
+function divideBtnListener(){
+    console.log('hello from divide button:', divideBtnListener);
+   
+}
+
+function equalsBtnListener(){
+    console.log('hello from equals button:', equalsBtnListener);
+   
+}
+
+function cBtnListener(){
+    console.log('hello from C button:', cBtnListener);
+   
+}
 
