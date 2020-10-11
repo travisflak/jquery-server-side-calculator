71a211ec1e4f08a475a221ee6418941591c13c06
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-10 19:22:27 -0500

    localhost 5000 iserver is sucessfully running

--- server/server.js
+++ server/server.js
@@ -4,3 +4,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
+
+
+
+
+
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT)
+});
