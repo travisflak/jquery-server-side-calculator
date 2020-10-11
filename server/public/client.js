8647cccab1dd6034962fec9b2f4134e53455cf31
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-10-10 21:35:08 -0500

    created click listeners for buttons but still need to create needed functions

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,13 @@
 $(document).ready(onReady);
 
 function onReady() {
-    
+
+
+    $('#plus').on('click', plusBtnListener);
+    // $('#minus').on('click', postPlayerData);
+    // $('#times').on('click', postPlayerData);
+    // $('#divide').on('click', postPlayerData);
+    // $('#equals').on('click', postPlayerData);
+    // $('#C').on('click', postPlayerData);
 }
+
